936eb60f99fb6e6848198a19ef9ad3ca498aee2a
xingyuzhu-xy <443243855@qq.com>
2016-12-14 14:35:43 +0800

    zxy

---
+++ ZGZ/app/src/main/java/test/leco/com/zgz/zxy/MyCollectActivity.java
@@ -0,0 +1,18 @@
+package test.leco.com.zgz.zxy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import test.leco.com.zgz.R;
+
+/**
+ * Created by Administrator on 2016/12/14.
+ */
+
+public class MyCollectActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_my_collect_layout);
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ ZGZ/app/src/main/java/test/leco/com/zgz/zxy/MyMessageActivity.java
@@ -0,0 +1,18 @@
+package test.leco.com.zgz.zxy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import test.leco.com.zgz.R;
+
+/**
+ * Created by Administrator on 2016/12/14.
+ */
+
+public class MyMessageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_my_message_layout);
+        super.onCreate(savedInstanceState);
+    }
+}
