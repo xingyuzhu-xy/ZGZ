f69593d6b80dba14610a0398c60516d3f355e08c
lijunming <you@example.com>
2016-12-19 09:37:24 +0800

    补全页面

--- ZGZ/app/src/main/java/test/leco/com/zgz/t/fragment/InterviewFragment.java
+++ ZGZ/app/src/main/java/test/leco/com/zgz/t/fragment/InterviewFragment.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import test.leco.com.zgz.R;
 import test.leco.com.zgz.t.MePositionActivity;
 import test.leco.com.zgz.zxy.InterviewFaceActivity;
+import test.leco.com.zgz.zxy.InterviewMessageActivity;
 
 /**
  * Created by Administrator on 2016/12/14.
@@ -25,8 +26,10 @@ public class InterviewFragment extends Fragment {
         View view = inflater.inflate(R.layout.activity_interview_layout,null);
         TextView  interview = (TextView) view.findViewById(R.id.interview);
         LinearLayout mePosition = (LinearLayout) view.findViewById(R.id.me_position);
+        LinearLayout positionMe = (LinearLayout) view.findViewById(R.id.position_me);
         mePosition.setOnClickListener(clickListener);
         interview.setOnClickListener(clickListener);
+        positionMe.setOnClickListener(clickListener);
         return view;
     }
 
@@ -42,6 +45,10 @@ public class InterviewFragment extends Fragment {
                     break;
                 case R.id.me_position:
                     intent = new Intent(getActivity(), MePositionActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.position_me:
+                    intent = new Intent(getActivity(), InterviewMessageActivity.class);
                     startActivity(intent);
                     break;
             }
--- ZGZ/app/src/main/java/test/leco/com/zgz/zxy/ReferCompanyActivity.java
+++ ZGZ/app/src/main/java/test/leco/com/zgz/zxy/ReferCompanyActivity.java
@@ -2,6 +2,8 @@ package test.leco.com.zgz.zxy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import test.leco.com.zgz.R;
 
@@ -10,9 +12,18 @@ import test.leco.com.zgz.R;
  */
 
 public class ReferCompanyActivity extends Activity {
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refer_company_layout);
+        back = (ImageView) findViewById(R.id.resume_arrow);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
