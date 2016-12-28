9307e10c4a1206223e69ce1a5ab85bbc009dc393
lijunming <you@example.com>
2016-12-28 11:41:13 +0800

    ...

--- ZGZ/app/src/main/java/test/leco/com/zgz/t/PositionDetailsActivity.java
+++ ZGZ/app/src/main/java/test/leco/com/zgz/t/PositionDetailsActivity.java
@@ -2,6 +2,7 @@ package test.leco.com.zgz.t;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,12 +20,14 @@ public class PositionDetailsActivity extends Activity{
     ImageView back; //返回上级页面
     TextView enterprise; //企业咨询
     int en_id; //企业的id
+    TextView phone; //电话
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.t_position_details);
         back = (ImageView) findViewById(R.id.back_icon);
         enterprise = (TextView) findViewById(R.id.enterprise_consultation);
+        phone = (TextView) findViewById(R.id.phone);
         //结束当前页面，返回上级页面
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +35,7 @@ public class PositionDetailsActivity extends Activity{
                 finish();
             }
         });
+        //跳转到企业咨询页面
         enterprise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,5 +46,15 @@ public class PositionDetailsActivity extends Activity{
         Intent inte = getIntent();
         en_id = inte.getIntExtra("en",-1);
         Log.i("en_id===>",""+en_id);
+        //打电话
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                Uri data = Uri.parse("tel:" + "");
+                intent.setData(data);
+                startActivity(intent);
+            }
+        });
     }
 }
